127e1a48ce488c8aff1e2217cd772a77496bb30e
David Chmura <dschmura@humbledaisy.com>
2018-08-17 16:31:55 -0400

    UPDATE: annotations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  uniqname   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
 end
