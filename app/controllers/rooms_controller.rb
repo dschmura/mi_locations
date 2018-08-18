a307d66140fc6f9a3dd821cbc574e4bc2034b5dc
David Chmura <dschmura@humbledaisy.com>
2018-08-17 22:40:06 -0400

    ADD: rooms controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,21 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:show]
+
+  def index
+    @rooms = Room.includes(:building).page params[:page]
+  end
+
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_room
+      @room = Room.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def room_params
+      params.require(:room).permit(:rmrecnbr, :latitude, :longitude, :floor, :room_number, :facility_code_heprod, :rmtyp_description, :dept_id, :dept_grp, :square_feet, :instructional_seating_count, :building_id)
+    end
+end
