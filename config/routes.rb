08f9e561c2f5ce1faf5f3f6b9506cc233c03a322
David Chmura <dschmura@humbledaisy.com>
2018-08-17 22:39:39 -0400

    ADD: building controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :rooms, only: [:show, :index]
+  resources :buildings, only: [:show, :index]
+
   get '/project_status', to: 'pages#project_status'
   get '/privacy', to: 'pages#privacy'
   get '/contact', to: 'pages#contact'
