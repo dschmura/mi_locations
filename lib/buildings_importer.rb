cc5c50256c754f7b687422e7100c584fcb08caee
David Chmura <dschmura@humbledaisy.com>
2018-08-23 11:39:07 -0400

    UPDATE: Building importer
    Remove extra spaces from building addresses.

--- lib/buildings_importer.rb
+++ lib/buildings_importer.rb
@@ -34,21 +34,21 @@ class BuildingsImporter
   end
 
   def update_geocoding(building)
-    if building.address != ("#{row['BLDSTREETNBR']}  #{row['BLDSTREETDIR']}  #{row['BLDSTREETNAME']}" ).strip
+    if building.address != ("#{row['BLDSTREETNBR']}  #{row['BLDSTREETDIR']}  #{row['BLDSTREETNAME']}" ).gsub(/\s+/, " ")
       GeocodeBuildingJob.perform_later(@building.id)
     end
   end
 
   def update_building(row)
     @building = Building.find_by(bldrecnbr: row['BLDRECNBR'].to_i)
-      if @building.update(bldrecnbr: row["BLDRECNBR"], name: row["BLD_DESCR50"], nick_name: row["BLD_DESCR"], abbreviation: row["BLD_DESCRSHORT"], address:  (" #{row['BLDSTREETNBR']}  #{row['BLDSTREETDIR']}  #{row['BLDSTREETNAME']}" ).strip , city: row["BLDCITY"], state: row["BLDSTATE"], zip: row["BLDPOSTAL"] , country: row["BLDCOUNTRY"] )
+      if @building.update(bldrecnbr: row["BLDRECNBR"], name: row["BLD_DESCR50"], nick_name: row["BLD_DESCR"], abbreviation: row["BLD_DESCRSHORT"], address:  (" #{row['BLDSTREETNBR']}  #{row['BLDSTREETDIR']}  #{row['BLDSTREETNAME']}" ).strip.gsub(/\s+/, " ") , city: row["BLDCITY"], state: row["BLDSTATE"], zip: row["BLDPOSTAL"] , country: row["BLDCOUNTRY"] )
         # puts "UPDATED #{row['BLDRECNBR']}"
       GeocodeBuildingJob.perform_later(@building.id)
     end
   end
 
   def create_building(row)
-    @building = Building.new(bldrecnbr: row["BLDRECNBR"], name: row["BLD_DESCR50"], nick_name: row["BLD_DESCR"], abbreviation: row["BLD_DESCRSHORT"], address:  ("#{row['BLDSTREETNBR']}  #{row['BLDSTREETDIR']}  #{row['BLDSTREETNAME']}" ).strip , city: row["BLDCITY"], state: row["BLDSTATE"], zip: row["BLDPOSTAL"] , country: row["BLDCOUNTRY"] )
+    @building = Building.new(bldrecnbr: row["BLDRECNBR"], name: row["BLD_DESCR50"], nick_name: row["BLD_DESCR"], abbreviation: row["BLD_DESCRSHORT"], address:  ("#{row['BLDSTREETNBR']}  #{row['BLDSTREETDIR']}  #{row['BLDSTREETNAME']}" ).strip.gsub(/\s+/, " ") , city: row["BLDCITY"], state: row["BLDSTATE"], zip: row["BLDPOSTAL"] , country: row["BLDCOUNTRY"] )
 
     if @building.save
       GeocodeBuildingJob.perform_later(@building.id)
