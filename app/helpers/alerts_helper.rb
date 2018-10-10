bdb1c56acbb758128d53a72c3cd2b4bd71bce4cf
David Chmura <dschmura@humbledaisy.com>
2018-10-10 14:41:44 -0400

    ADD: Icons to reflect the severity of a warning.

---
+++ app/helpers/alerts_helper.rb
@@ -0,0 +1,6 @@
+module AlertsHelper
+  SEVERITY_ICONS = {info: 'info-circle', danger: 'exclamation-triangle', warning: 'bullhorn', success: 'check-circle' }
+  def severity_icon(severity)
+    SEVERITY_ICONS[severity]
+  end
+end
