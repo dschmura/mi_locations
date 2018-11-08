3fc77f15bbb20404cbe80cedf714021b2f942ca1
David Chmura <dschmura@humbledaisy.com>
2018-11-08 10:30:16 -0500

    FIX: clear_dev_logs initializer
    Had wrong quotes.

--- config/initializers/clear_dev_logs.rb
+++ config/initializers/clear_dev_logs.rb
@@ -5,6 +5,6 @@ if Rails.env.devlopment?
   logs = File.join(Rails.root, 'log', '*.log')
   if Dir[logs].any? { |log| File.size?(log).to_i > MAX_LOG_SIZE }
     $stdout.puts 'Running rails log:clear'
-    `rails log:clear`
+    'rails log:clear'
   end
 end
