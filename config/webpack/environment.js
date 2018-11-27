d7e8becb2ebe813c71f341cc9c54edefa48f69c6
David Chmura <dschmura@humbledaisy.com>
2018-11-27 16:50:17 -0500

    FIX: postcssrc reference issue.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// For production deployments?
+const path = require("path")
+Object.assign(environment.loaders.get("css").use.find(el => el.loader === "postcss-loader").options, {
+  config: {
+    path: path.resolve(__dirname, "../..", ".postcssrc.yml")
+  }
+})
