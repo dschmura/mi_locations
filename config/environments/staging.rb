5b3a26289c450e94f1fcc846951de4e6ded08819
David Chmura <dschmura@humbledaisy.com>
2018-12-04 14:29:48 -0500

    UPDATE: Configs to try and get email working in staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "lsa-mis-rails-staging2.miserver.it.umich.edu" }
   config.action_mailer.smtp_settings = {
     address:              Rails.application.credentials.MI_LOCATIONS_EMAIL_SERVER,
     domain:               Rails.application.credentials.MI_LOCATIONS_EMAIL_DOMAIN,
