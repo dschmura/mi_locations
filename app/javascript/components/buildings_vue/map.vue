85cc2dd9912e43801cc6e1d82eb8b3b633a5fc56
David Chmura <dschmura@humbledaisy.com>
2019-02-04 10:31:02 -0500

    ADD: Building Vue

---
+++ app/javascript/components/buildings_vue/map.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="test-map">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+// import general map component
+export default {
+  data: function () {
+    return {
+      message: "Hello Buildings Map!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ app/javascript/packs/buildings_vue.js
@@ -0,0 +1,35 @@
+import Vue from 'vue/dist/vue.esm'
+
+import TurbolinksAdapter from 'vue-turbolinks'
+Vue.use(TurbolinksAdapter)
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+import * as Clipboard from 'clipboard'
+
+import App from '../components/app.vue'
+Vue.component('app', App)
+
+import BuildingsMap from '../components/buildings_vue/map.vue'
+Vue.component('buildings-map', BuildingsMap)
+
+Vue.config.ignoredElements = ['trix-toolbar', 'trix-editor']
+// import Map from '../components/buildings_maps.vue'
+// Vue.component('buildings_maps', Map)
+
+// import * as VueGoogleMaps from 'vue2-google-maps'
+
+document.addEventListener('turbolinks:load', () => {
+  Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+  const app = new Vue({
+    el: '[data-behavior="vue"]',
+    data: {
+      message: "I am a building vue?"
+    },
+    components: {
+      BuildingsMap
+    }
+  })
+})
