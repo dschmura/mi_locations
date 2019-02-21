53f20d95f53d722d815273a8c696ba4eb88def65
David Chmura <dschmura@humbledaisy.com>
2019-02-21 09:54:52 -0500

    UPDATE: ran rails update

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_203409) do
+ActiveRecord::Schema.define(version: 2019_02_21_145134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,15 @@ ActiveRecord::Schema.define(version: 2018_12_14_203409) do
     t.index ["building_id"], name: "index_floors_on_building_id"
   end
 
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.integer "age"
+    t.string "favorite_treat"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "room_characteristics", force: :cascade do |t|
     t.integer "rmrecnbr"
     t.integer "chrstc"
@@ -142,6 +151,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_203409) do
     t.index ["uniqname"], name: "index_users_on_uniqname", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "floors", "buildings"
   add_foreign_key "room_characteristics", "rooms"
   add_foreign_key "room_contacts", "rooms"
