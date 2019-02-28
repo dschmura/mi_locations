c09e24895b099b87c338c4a2bcf27663f0ddcca3
David Chmura <dschmura@humbledaisy.com>
2019-02-28 10:23:35 -0500

    ADD: Routes for lsa and about_iss.

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,8 @@ Rails.application.routes.draw do
   get 'pages/contact'
   get 'pages/privacy'
   get 'pages/project_status'
+  get 'lsa' => redirect('https://lsa.umich.edu')
+  get 'about_iss' => redirect('https://lsa.umich.edu')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources 'feedbacks', only: [:create]
 end
