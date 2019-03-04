c18dac231eb832168ed3e008e2206edee3bff106
David Chmura <dschmura@humbledaisy.com>
2019-03-04 10:22:02 -0500

    UPDATE: Add default order to classroom index action while working on design"

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,11 +1,14 @@
 class ClassroomsController < RoomsController
   def index
     @q = Room.classrooms.ransack(params[:q])
-    @rooms = @q.result.includes(:building, :room_characteristics, :room_contact).page params[:page]
+    @rooms = @q.result.includes(:building, :room_characteristics, :room_contact).order(id: :asc).page params[:page]
   end
 
+  private
+
   def set_room
-      # @room = Room.find(params[:id])
-      @room = Room.find_by facility_code_heprod:(params[:id].upcase)
-    end
+    # @room = Room.find(params[:id])
+    @room = Room.find_by facility_code_heprod:(params[:id].upcase)
+  end
+
 end
