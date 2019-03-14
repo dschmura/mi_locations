3b6d3039a271d3be821a52b99339ebd184dc951f
David Chmura <dschmura@humbledaisy.com>
2019-03-14 10:39:56 -0400

    ADD: Attributes file (for specs using Rick's method for testing)

---
+++ spec/support/attributes.rb
@@ -0,0 +1,15 @@
+def building_attributes(overrides = {})
+  {
+    bldrecnbr: 1234567,
+    latitude: 42.2785133809524,
+    longitude: -83.7402616825397,
+    name: "TESTING BUILDNG NAMED FOR BIG DONER",
+    nick_name: "TESTING BUILDING",
+    abbreviation: "TEST",
+    address: "123 TEST AVE",
+    city: "ANN ARBOR",
+    state: "MI",
+    zip: "48109-1055",
+    country: "USA"
+  }.merge(overrides)
+end
