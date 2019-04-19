584ebf704209db1b81729c5e0f1e98da326517e7
David Chmura <dschmura@humbledaisy.com>
2019-04-19 13:09:44 -0400

    UPDATE: Classrooms index now returns distinct records

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,7 +1,7 @@
 class ClassroomsController < RoomsController
   def index
     @q = Room.classrooms.ransack(params[:q])
-    @rooms = @q.result.includes(:building, :room_characteristics, :room_contact).order(id: :asc).page params[:page]
+    @rooms = @q.result(distinct: true).includes(:building, :room_characteristics, :room_contact).order(id: :asc).page params[:page]
   end
 
   private
