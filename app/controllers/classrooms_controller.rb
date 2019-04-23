222540e3eec3e373f500e96693dc1d114c12b9f0
David Chmura <dschmura@humbledaisy.com>
2019-04-23 08:59:47 -0400

    ADD: index responds to json requests now.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -2,6 +2,13 @@ class ClassroomsController < RoomsController
   def index
     @q = Room.classrooms.ransack(params[:q])
     @rooms = @q.result(distinct: true).includes(:building, :room_characteristics, :room_contact).order(id: :asc).page params[:page]
+
+    respond_to do |format|
+
+      format.html # show.html.erb
+      format.json { render json: @rooms }
+
+    end
   end
 
   private
