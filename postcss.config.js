8ec56f6d1edbda75dc0c247c14275f41cc67b85e
David Chmura <dschmura@humbledaisy.com>
2019-04-27 07:23:15 -0400

    ADD: Purge-css

--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,14 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
   ]
+}
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./app/**/*.html.erb', './app/**/*.html.haml','./app/helpers/**/*.rb'],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
 }
