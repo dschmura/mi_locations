6a22a1564f0fb225a5c181319210d3a8c6920147
David Chmura <dschmura@humbledaisy.com>
2019-05-21 14:27:40 -0400

    UPDATE: Filters for search making progress.

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -138,6 +138,7 @@ module.exports = {
 
       addComponents({
         '.form-checkbox': {
+          appearance: 'none',
           display: 'inline-block',
           height: theme('spacing.4', defaultTheme.spacing[4]),
           width: theme('spacing.4', defaultTheme.spacing[4]),
@@ -146,23 +147,24 @@ module.exports = {
           borderRadius: theme('borderRadius.default', defaultTheme.borderRadius.default),
           backgroundColor: '#fff',
           userSelect: 'none',
-          'input[type=checkbox]:focus + &': {
+          'input[type=checkbox]:focus + &, input[type=checkbox]&:focus': {
             outline: 'none',
             boxShadow: theme('boxShadow.outline', defaultTheme.boxShadow.outline),
           },
-          'input[type=checkbox]:focus:not(:checked) + &': {
+          'input[type=checkbox]:focus:not(:checked) + &, input[type=checkbox]&:focus:not(:checked)': {
             borderColor: defaultTheme.colors.blue[400],
           },
-          'input[type=checkbox]:checked + &': {
+          'input[type=checkbox]:checked + &, input[type=checkbox]&:checked': {
             backgroundColor: 'currentColor',
             borderColor: 'currentColor',
-            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 24 24' fill='%23ffffff' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M10.5 14.586l7.293-8.293a1 1 0 0 1 1.414 1.414l-8 9a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 1.414-1.414l3.293 3.293z' fill-rule='evenodd'/%3E%3C/svg%3E")`,
+            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 16 16' fill='%23fff' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M13.293 4.293a1 1 0 0 1 0 1.414L7 12a1 1 0 0 1-1.414 0L3.293 9.707a1 1 0 0 1 1.414-1.414l1.586 1.586 5.586-5.586a1 1 0 0 1 1.414 0z'/%3E%3C/svg%3E")`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
           },
         },
         '.form-radio': {
+          appearance: 'none',
           display: 'inline-block',
           height: theme('spacing.4', defaultTheme.spacing[4]),
           width: theme('spacing.4', defaultTheme.spacing[4]),
@@ -171,14 +173,14 @@ module.exports = {
           borderRadius: '9999px',
           backgroundColor: '#fff',
           userSelect: 'none',
-          'input[type=radio]:focus + &': {
+          'input[type=radio]:focus + &, input[type=radio]&:focus': {
             outline: 'none',
             boxShadow: theme('boxShadow.outline', defaultTheme.boxShadow.outline),
           },
-          'input[type=radio]:focus:not(:checked) + &': {
+          'input[type=radio]:focus:not(:checked) + &, input[type=radio]&:focus:not(:checked)': {
             borderColor: defaultTheme.colors.blue[400],
           },
-          'input[type=radio]:checked + &': {
+          'input[type=radio]:checked + &, input[type=radio]&:checked': {
             backgroundColor: 'currentColor',
             borderColor: 'currentColor',
             backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 24 24' fill='%23ffffff' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='12' cy='12' r='5'/%3E%3C/svg%3E")`,
@@ -213,7 +215,7 @@ module.exports = {
           backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='${theme('colors.gray.500', defaultTheme.colors.gray[500]).replace('#', '%23')}' viewBox='0 0 24 24' %3E%3Cpath d='M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z'/%3E%3C/svg%3E")`,
           backgroundPosition: `right ${theme('spacing.2', defaultTheme.spacing[2])} center`,
           backgroundRepeat: 'no-repeat',
-          backgroundSize: '1.5em',
+          backgroundSize: '1.5em 1.5em',
           '&::-ms-expand': {
             display: 'none',
           },
