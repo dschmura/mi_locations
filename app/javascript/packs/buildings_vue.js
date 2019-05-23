8e6df6386b25fd9442295407c72a2182f58ffa06
David Chmura <dschmura@humbledaisy.com>
2019-05-22 22:22:46 -0400

    REFACTOR: Remove vue and yarn and bundle updates

--- app/javascript/packs/buildings_vue.js
+++ app/javascript/packs/buildings_vue.js
@@ -1,60 +1,60 @@
-import Vue from 'vue/dist/vue.esm'
+// import Vue from 'vue/dist/vue.esm'
 
-import TurbolinksAdapter from 'vue-turbolinks'
-Vue.use(TurbolinksAdapter)
+// import TurbolinksAdapter from 'vue-turbolinks'
+// Vue.use(TurbolinksAdapter)
 
-// import VueResource from 'vue-resource'
-// Vue.use(VueResource)
-
-
-// import Search from '../components/search.vue'
-// Vue.component('search', Search)
-
-Vue.config.ignoredElements = ['trix-toolbar', 'trix-editor']
-// import Map from '../components/buildings_maps.vue'
-// Vue.component('buildings_maps', Map)
-
-// import * as VueGoogleMaps from 'vue2-google-maps'
-// Vue.component('buildings_maps', VueGoogleMaps)
-
-FontAwesomeConfig = { autoReplaceSvg: 'nest' }//important addition!
+// // import VueResource from 'vue-resource'
+// // Vue.use(VueResource)
 
 
-document.addEventListener('turbolinks:load', () => {
-  // Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+// // import Search from '../components/search.vue'
+// // Vue.component('search', Search)
 
-  const app = new Vue({
+// Vue.config.ignoredElements = ['trix-toolbar', 'trix-editor']
+// // import Map from '../components/buildings_maps.vue'
+// // Vue.component('buildings_maps', Map)
 
-    el: '[data-behavior="vue-layout"]',
-    data: {
-      layout: 'list',
-      filters: false,
-      sort_options: false,
-      room_characteristics_params: []
-    },
-    methods: {
-      set_layout_list() {
-        this.layout = 'list';
-      },
-      set_layout_grid() {
-        this.layout = 'grid';
-      },
-      set_layout_map() {
-        this.layout = 'map';
-      },
-      toggle_filters() {
-        this.filters = this.filters ? false : true;
-      },
-      toggle_sort_options() {
-        this.sort_options = this.sort_options ? false : true;
-      },
-      clear_room_characteristics_params(){
-        this.room_characteristics_params = [];
-      },
-      update_results_on_change() {
-        // axios submit room_characteristics_chrstc_descrshort_cont_all(this.room_characteristics_params)
-      }
-    }
-  });
+// // import * as VueGoogleMaps from 'vue2-google-maps'
+// // Vue.component('buildings_maps', VueGoogleMaps)
 
-});
+// FontAwesomeConfig = { autoReplaceSvg: 'nest' }//important addition!
+
+
+// document.addEventListener('turbolinks:load', () => {
+//   // Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+//   const app = new Vue({
+
+//     el: '[data-behavior="vue-layout"]',
+//     data: {
+//       layout: 'list',
+//       filters: false,
+//       sort_options: false,
+//       room_characteristics_params: []
+//     },
+//     methods: {
+//       set_layout_list() {
+//         this.layout = 'list';
+//       },
+//       set_layout_grid() {
+//         this.layout = 'grid';
+//       },
+//       set_layout_map() {
+//         this.layout = 'map';
+//       },
+//       toggle_filters() {
+//         this.filters = this.filters ? false : true;
+//       },
+//       toggle_sort_options() {
+//         this.sort_options = this.sort_options ? false : true;
+//       },
+//       clear_room_characteristics_params(){
+//         this.room_characteristics_params = [];
+//       },
+//       update_results_on_change() {
+//         // axios submit room_characteristics_chrstc_descrshort_cont_all(this.room_characteristics_params)
+//       }
+//     }
+//   });
+
+// });
--- app/javascript/packs/search_vue.js
+++ app/javascript/packs/search_vue.js
@@ -1,71 +1,71 @@
-import Vue from 'vue/dist/vue.esm'
+// import Vue from 'vue/dist/vue.esm'
 
-import TurbolinksAdapter from 'vue-turbolinks'
-Vue.use(TurbolinksAdapter)
+// import TurbolinksAdapter from 'vue-turbolinks'
+// Vue.use(TurbolinksAdapter)
 
-import axios from 'axios'
+// import axios from 'axios'
 
-let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
-axios.defaults.headers.common['X-CSRF-Token'] = token
-axios.defaults.headers.common['Accept'] = 'application/json'
+// let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
+// axios.defaults.headers.common['X-CSRF-Token'] = token
+// axios.defaults.headers.common['Accept'] = 'application/json'
 
-// import Search from '../components/search.vue'
-// Vue.component('search', Search)
+// // import Search from '../components/search.vue'
+// // Vue.component('search', Search)
 
-// import RoomsFilters from '../components/rooms_filters.vue'
-// Vue.component('rooms_filters', RoomsFilters)
-import LayoutButton from '../components/layout_button.vue'
-Vue.component('layout-button', LayoutButton)
+// // import RoomsFilters from '../components/rooms_filters.vue'
+// // Vue.component('rooms_filters', RoomsFilters)
+// import LayoutButton from '../components/layout_button.vue'
+// Vue.component('layout-button', LayoutButton)
 
-import vSelect from 'vue-select'
-Vue.component('v-select', vSelect)
-import 'vue-select/src/scss/vue-select.scss'
+// import vSelect from 'vue-select'
+// Vue.component('v-select', vSelect)
+// import 'vue-select/src/scss/vue-select.scss'
 
-Vue.config.ignoredlements = ['trix-toolbar', 'trix-editor']
+// Vue.config.ignoredlements = ['trix-toolbar', 'trix-editor']
 
-FontAwesomeConfig = { autoReplaceSvg: 'nest' }//important addition!
+// FontAwesomeConfig = { autoReplaceSvg: 'nest' }//important addition!
 
-document.addEventListener('turbolinks:load', () => {
+// document.addEventListener('turbolinks:load', () => {
 
-  const app = new Vue({
+//   const app = new Vue({
 
-    el: '[data-behavior="vue-layout"]',
-    // props: ['isOpen'],
-    props: {
+//     el: '[data-behavior="vue-layout"]',
+//     // props: ['isOpen'],
+//     props: {
 
 
-    },
-    props: {
-      isOpen: {
-        type: Boolean,
-        default: false
-      },
-      isCard: {
-        type: Boolean,
-        default: false
-      },
-      isRow: {
-        type: Boolean,
-        default: true
-      },
-      placeholder: {
-        type: String,
-        default: "HI Dave"
-      },
-    },
-    data: function () {
-      return {
-        room_characteristics_params: [],
-        buildings: [],
-        schools_or_colleges: []
-      }
-    },
-    methods: {
-      clear_room_characteristics_params() {
-        this.room_characteristics_params = [];
-      }
-    },
+//     },
+//     props: {
+//       isOpen: {
+//         type: Boolean,
+//         default: false
+//       },
+//       isCard: {
+//         type: Boolean,
+//         default: false
+//       },
+//       isRow: {
+//         type: Boolean,
+//         default: true
+//       },
+//       placeholder: {
+//         type: String,
+//         default: "HI Dave"
+//       },
+//     },
+//     data: function () {
+//       return {
+//         room_characteristics_params: [],
+//         buildings: [],
+//         schools_or_colleges: []
+//       }
+//     },
+//     methods: {
+//       clear_room_characteristics_params() {
+//         this.room_characteristics_params = [];
+//       }
+//     },
 
-  });
+//   });
 
-});
+// });
