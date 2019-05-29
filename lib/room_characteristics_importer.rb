6f0cda808c7a216689d3e95dd62343eb64833e1e
David Chmura <dschmura@humbledaisy.com>
2019-05-29 15:37:02 -0400

    UPDATE: removed specific date

--- lib/room_characteristics_importer.rb
+++ lib/room_characteristics_importer.rb
@@ -16,7 +16,7 @@ class RoomCharacteristicsImporter
                 "CHRSTC_DESCR254" => :chrstc_desc254}.freeze
 
   def initialize
-    file = find_file("uploads/Imports_04_04_2019/room_characteristics.csv")
+    file = find_file("uploads/room_characteristics.csv")
     # @rmrecnbrs = Room.all.pluck(:rmrecnbr)
     @classrooms = Room.classrooms.group_by(&:rmrecnbr)
     @room_characteristics = load_room_characteristics_from_csv(file)
