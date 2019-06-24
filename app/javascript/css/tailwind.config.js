52ce992f25a02b7c0edb822aee1c244be9b00379
David Chmura <dschmura@humbledaisy.com>
2019-06-24 08:59:13 -0400

    UPDATE: Tailwindcss-forms

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -112,35 +112,38 @@ module.exports = {
       },
     },
     customForms: theme => ({
-      // horizontalPadding: defaultTheme.spacing[3],
-      // verticalPadding: defaultTheme.spacing[2],
-      lineHeight: theme('lineHeight.snug'),
-      // fontSize: defaultTheme.fontSize.base,
-      borderColor: 'transparent',
-      // borderWidth: defaultTheme.borderWidth.default,
-      borderRadius: theme('borderRadius.lg'),
-      backgroundColor: theme('colors.gray.700'),
-      focusBorderColor: 'transparent',
-      focusShadow: 'none',
-      // boxShadow: defaultTheme.boxShadow.none,
-      checkboxSize: '1.3em',
-      radioSize: '1.3em',
-      // checkboxIcon: `<svg viewBox="0 0 16 16" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path d="M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z"/></svg>`,
-      // radioIcon: `<svg viewBox="0 0 16 16" fill="#fff" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="3"/></svg>`,
-      checkedColor: theme('colors.blue.800'),
-      selectIcon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${defaultTheme.colors.blue[100]}"><path d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"/></svg>`,
-      // selectIconOffset: defaultTheme.spacing[2],
-      // selectIconSize: '1.5em',
+      default: {
+        input: {
+          borderRadius: theme('borderRadius.lg'),
+          backgroundColor: theme('colors.gray.200'),
+          '&:focus': {
+            backgroundColor: theme('colors.white'),
+          }
+        },
+        select: {
+          borderRadius: theme('borderRadius.lg'),
+          boxShadow: theme('boxShadow.default'),
+        },
+        'input, textarea, multiselect, checkbox, radio': {
+          backgroundColor: theme('colors.gray.900'),
+        },
+        checkbox: {
+          width: theme('spacing.6'),
+          height: theme('spacing.6'),
+          '&:checked': {
+            iconColor:  theme('colors.yellow.500'),
+
+          },
+
+        }
+      }
     })
+
 
   },
   variants: {
     // Some useful comment
   },
-  plugins: [
-    // Some useful comment
-  ],
-
   plugins: [
     require('@tailwindcss/custom-forms')
   ]
