738faddee8b645705b4129a075dbc41413800d55
David Chmura <dschmura@humbledaisy.com>
2019-06-27 15:57:34 -0400

    ADD: User actions dropdown
    Clicking on the user icon now exposes a dropdown on breakpoint small and larger.

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,16 +1,27 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "mainnav", "filters"]
+  static targets = [ "dropdown", "filters", "mainnav"]
 
 
-  toggle() {
+  mainnavtoggle() {
     event.preventDefault()
     this.mainnavTargets.forEach((el, i) => {
       el.classList.toggle("hidden")
 
     })
   }
+
+  dropdowntoggle() {
+    const { clientWidth } = this.element
+    event.preventDefault()
+    if (clientWidth > 640 ){
+      this.dropdownTargets.forEach((el, i) => {
+        el.classList.toggle("hidden")
+      })
+    }
+  }
+
   filtertoggle() {
     event.preventDefault()
     this.filtersTargets.forEach((el, i) => {
