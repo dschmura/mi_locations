a3a5cf5a3df7647d5439e912532fd4696f68855f
David Chmura <dschmura@humbledaisy.com>
2019-07-06 10:04:09 -0400

    ADD: Maps v0.0.1

--- app/javascript/controllers/roomsmaps_controller.js
+++ app/javascript/controllers/roomsmaps_controller.js
@@ -1,16 +1,29 @@
-const loadGoogleMapsApi = require('load-google-maps-api');
-class Map {
+// const loadGoogleMapsApi = require('load-google-maps-api');
+// class Map {
+//   static loadGoogleMapsApi() {
+//     // TODO
+//     // return loadGoogleMapsApi({ key: "<%= Rails.application.credentials[:google_geocoder_api_key] %>" });
+//     return loadGoogleMapsApi({ key: "AIzaSyDanqx5Jg7UKmeh9tTVeV6ovANhFhxOFhk" });
+//   }
+//   static createMap(googleMaps, mapElement) {
+//     return new googleMaps.Map(mapElement, {
+//       center: { lat: 46.276868, lng: -83.738206 },
+//       zoom: 15
+//     });
+//   }
+// }
+// export { Map };
+// // console.log( "<%= Rails.application.credentials.google_geocoder_api_key />" )
 
-  static loadGoogleMapsApi() {
-    // TODO
-    return loadGoogleMapsApi({ key: "<%= Rails.application.credentials[:google_geocoder_api_key] %>" });
 
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "map"]
+  initialize() {
+    fetch(this.data.get("gmap"))
+    console.log("HI")
+    console.log(gmap)
   }
-  static createMap(googleMaps, mapElement) {
-    return new googleMaps.Map(mapElement, {
-      center: { lat: 42.276868, lng: -83.738206 },
-      zoom: 14
-    });
-  }
+
 }
-export { Map };
