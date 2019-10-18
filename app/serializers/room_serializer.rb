5952f73c5f9d0ba1060b9284c09663ba4535e1bf
David Chmura <dschmura@humbledaisy.com>
2019-10-18 05:52:49 -0400

    UPDATE: ran annotate

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id                          :bigint           not null, primary key
+#  rmrecnbr                    :integer          not null
+#  latitude                    :float
+#  longitude                   :float
+#  floor                       :string           not null
+#  room_number                 :string           not null
+#  facility_code_heprod        :string           not null
+#  rmtyp_description           :string
+#  dept_id                     :integer
+#  dept_grp                    :string
+#  square_feet                 :integer
+#  instructional_seating_count :integer
+#  building_id                 :bigint
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  visible                     :boolean          default(FALSE), not null
+#  dept_description            :string
+#
+
 class RoomSerializer < ActiveModel::Serializer
   attributes :id, :rmrecnbr, :floor, :room_number, :facility_code_heprod, :dept_grp, :instructional_seating_count, :dept_description, :building
 
