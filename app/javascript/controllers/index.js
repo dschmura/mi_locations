b7455f59b4a75119e193529c447d1914afd76a06
David Chmura <dschmura@humbledaisy.com>
2019-10-22 15:40:59 -0400

    ADD: Feedback modal via tailwindcss-stimulus-components

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,3 +7,10 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+// Import and register all TailwindCSS Components
+import { Dropdown, Modal, Tabs, Popover } from "tailwindcss-stimulus-components"
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
