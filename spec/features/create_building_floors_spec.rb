b896be620a476a1ab35057486741acf47242d121
David Chmura <dschmura@humbledaisy.com>
2019-11-14 05:29:03 -0500

    REFACTOR: Remove old test

--- spec/features/create_building_floors_spec.rb
+++ spec/features/create_building_floors_spec.rb
@@ -1,23 +1,23 @@
 require "rails_helper"
 
 feature "Create building floor" do
-  let(:building) { create(:building) }
+  # let(:building) { create(:building) }
 
-  scenario "filling in add a floor form" do
-    visit new_building_floor_path(building)
-    expect(page).to have_content("Add Floor")
+  # scenario "filling in add a floor form" do
+  #   visit new_building_floor_path(building)
+  #   # expect(page).to have_content("Add Floor")
 
-    fill_in "building_floor[floor_label]", with: "12"
-    click_button "Create Floor"
+  #   fill_in "building_floor[floor_label]", with: "12"
+  #   click_button "Create Floor"
 
-    expect(page).to have_content("BuildingFloor was successfully created")
-    expect(BuildingFloor.count).to eq 1
-    building_floor = BuildingFloor.last
+  #   expect(page).to have_content("BuildingFloor was successfully created")
+  #   expect(BuildingFloor.count).to eq 1
+  #   building_floor = BuildingFloor.last
 
-    expect(building_floor).to have_attributes(
-      building_id: building.id
-    )
-  end
+  #   expect(building_floor).to have_attributes(
+  #     building_id: building.id
+  #   )
+  # end
   # describe "Valid Buildings: " do
   #   it { should validate_presence_of(:bldrecnbr) }
   #   it { should validate_presence_of(:name) }
