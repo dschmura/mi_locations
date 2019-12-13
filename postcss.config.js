91aad1b686cdda3b5e22d934f94389447b441c8f
David Chmura <dschmura@humbledaisy.com>
2019-12-13 11:17:52 -0500

    UPDATE: postcss to reduce size of stylesheets

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,26 @@
-var tailwindcss = require('tailwindcss');
 module.exports = {
   plugins: [
-    tailwindcss('./app/javascript/css/tailwind.config.js'),
+    require("tailwindcss")('./app/javascript/css/tailwind.config.js'),
     require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        './app/**/*.html.erb',
+        './app/**/*.html.haml',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.sass',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    }),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
     }),
-// require('@fullhuman/postcss-purgecss')({
-//       content: [
-//         './app/**/*.html.erb',
-//         './app/**/*.html.haml',
-//         './app/helpers/**/*.rb',
-//         './app/javascript/**/*.js',
-//         './app/javascript/**/*.vue',
-//         './app/javascript/**/*.sass',
-//       ],
-//       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-//     })
+
   ]
 
 }
