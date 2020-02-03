a345e494136395c010ee00fe3c707cc0e3c536cb
David Chmura <dschmura@humbledaisy.com>
2020-02-03 12:06:20 -0500

    REFACTOR: Replace FontAwesome library with individual icon partials from svg
    This refactor allowed us to remove fontawesome in favor of loading individual icons (svgs) as rails partials. This change brought our compiled pack from 3.62MB down to 2.49MB

--- app/helpers/rooms_search_helper.rb
+++ app/helpers/rooms_search_helper.rb
@@ -3,80 +3,65 @@ module RoomsSearchHelper
     (characteristics.pluck(:chrstc_descrshort) & ["TeamTables", "TeamTech", "TeamTables"]).present?
   end
   # Match room_characteristics to icons (fontawesome v5)
+
   ROOM_CHARACTERISTIC_ICONS = {
-    "AssistLis" => "fas fa-assistive-listening-systems",
-    "AudSeat" => "fas fa-chair",
-    "Blackout" => "far fa-lightbulb",
-    "BluRay" => "fas fa-compact-disc",
-    "BluRay/DVD" => "fas fa-compact-disc",
-    "CaptionDev" => "fas fa-closed-captioning",
-    "Carpet" => "fas fa-info-circle",
-    "ChairFix" => "fas fa-chair",
-    "Chkbrd" => "fas fa-chalkboard-teacher",
-    "Chkbrd>25" => "fas fa-chalkboard-teacher",
-    "CompLabAny" => "fas fa-keyboard",
-    "CompLabMac" => "fas fa-keyboard",
-    "CompLabPC" => "fas fa-keyboard",
-    "CompPodMac" => "fab fa-apple",
-    "DocCam" => "fas fa-microscope",
-    "Ethernet" => "fas fa-ethernet",
-    "EthrStud" => "fas fa-ethernet",
-    "FloorTier" => "fas fa-info-circle",
-    "InstrComp" => "fas fa-laptop",
-    "IntrScreen" => "fas fa-edit",
-    "LectureCap" => "fas fa-podcast",
-    "MoveTablet" => "fas fa-info-circle",
-    "Platform" => "fas fa-info-circle",
-    "PowerStud" => "fas fa-plug",
-    "Proj16mm" => "fas fa-film",
-    "Proj35mm" => "fas fa-film",
-    "ProjD-Cin" => "fas fa-video",
-    "ProjDigit" => "fas fa-video",
-    "ProjSlide" => "fas fa-photo-video",
-    "SoundPrgrm" => "fas fa-volume-up",
-    "SoundVoice" => "fas fa-headset",
-    "Stage" => "fas fa-info-circle",
-    "TablesAny" => "fas fa-info-circle",
-    "TablesConf" => "fas fa-info-circle",
-    "TablesFix" => "fas fa-info-circle",
-    "TablesMov" => "fas fa-info-circle",
-    "TeamBoard" => "fas fa-users",
-    "TeamTables" => "fas fa-info-circle",
-    "TeamTech" => "fas fa-code-branch",
-    "Telephone" => "fas fa-phone",
-    "Tile" => "fas fa-info-circle",
-    "VCR" => "fas fa-info-circle",
-    "VideoConf" => "fas fa-satellite",
-    "WCInst" => "fas fa-wheelchair",
-    "Whtbrd" => "fas fa-chalkboard-teacher",
-    "Whtbrd>25" => "fas fa-chalkboard-teacher",
-    "Windows" => "fas fa-window-close",
-    "Wood" => "fas fa-info-circle",
+    "AssistLis" => "assistive-listening-systems",
+    "AudSeat" => "chair",
+    "Blackout" => "lightbulb",
+    "BluRay" => "compact-disc",
+    "BluRay/DVD" => "compact-disc",
+    "CaptionDev" => "closed-captioning",
+    "Carpet" => "info-circle",
+    "ChairFix" => "chair",
+    "Chkbrd" => "chalkboard-teacher",
+    "Chkbrd>25" => "chalkboard-teacher",
+    "CompLabAny" => "keyboard",
+    "CompLabMac" => "keyboard",
+    "CompLabPC" => "keyboard",
+    "CompPodMac" => "apple",
+    "DocCam" => "microscope",
+    "Ethernet" => "ethernet",
+    "EthrStud" => "ethernet",
+    "FloorTier" => "info-circle",
+    "InstrComp" => "laptop",
+    "IntrScreen" => "edit",
+    "LectureCap" => "podcast",
+    "MoveTablet" => "info-circle",
+    "Platform" => "info-circle",
+    "PowerStud" => "plug",
+    "Proj16mm" => "film",
+    "Proj35mm" => "film",
+    "ProjD-Cin" => "video",
+    "ProjDigit" => "video",
+    "ProjSlide" => "photo-video",
+    "SoundPrgrm" => "volume-up",
+    "SoundVoice" => "headset",
+    "Stage" => "info-circle",
+    "TablesAny" => "info-circle",
+    "TablesConf" => "info-circle",
+    "TablesFix" => "info-circle",
+    "TablesMov" => "info-circle",
+    "TeamBoard" => "users",
+    "TeamTables" => "info-circle",
+    "TeamTech" => "code-branch",
+    "Telephone" => "phone",
+    "Tile" => "info-circle",
+    "VCR" => "info-circle",
+    "VideoConf" => "satellite",
+    "WCInst" => "wheelchair",
+    "Whtbrd" => "chalkboard-teacher",
+    "Whtbrd>25" => "chalkboard-teacher",
+    "Windows" => "window-close",
+    "Wood" => "info-circle",
   }
 
-  def room_characteristic_icon(room_characteristic)
+  def room_icon(room_characteristic)
     icon = ROOM_CHARACTERISTIC_ICONS[room_characteristic.chrstc_descrshort]
-    "
-    <a role='button' class='characteristic tooltip tooltip-top' href='' data-tooltip='#{room_characteristic.chrstc_desc254}'>
-    <i class='fas fa-#{icon} fa-sm'></i>
-    </a>"
   end
 
-  def room_characteristic_chicklet(room_characteristic)
-    icon = ROOM_CHARACTERISTIC_ICONS[room_characteristic.chrstc_descrshort]
-    "
-    <a role='button' class='characteristic-chicklet tooltip tooltip-top' href='' data-tooltip='#{room_characteristic.chrstc_desc254}'>
-    <i class='fas fa-#{icon} fa-sm'></i>
-    <span>#{room_characteristic.chrstc_descrshort}
-    </span>
-    </a>"
+  def room_characteristic_icon_new(room_characteristic)
+    "#{ROOM_CHARACTERISTIC_ICONS[room_characteristic.chrstc_descrshort]}_icon"
   end
-
-  # def is_checked?(value)
-  #   if params[:q]
-  #     params[:q].each_value{ |k| k }.flatten(2).include?(value)
-  #   end
-  # end
 
   def is_checked?(values)
     if params[:q]
