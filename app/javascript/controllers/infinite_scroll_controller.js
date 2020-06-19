0b624839bf6dc6d9c4bbb25cb0d326c936faa069
David Chmura <dschmura@humbledaisy.com>
2020-06-19 12:21:45 -0400

    UPDATE: Infinite scroll to improve performance

--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -1,97 +1,46 @@
 import { Controller } from "stimulus"
-
+// import Rails from '@rails/ujs';
 export default class extends Controller {
-  static targets = [ "scrollContainer", "viewMore" ]
+  static targets = [ "entries", "pagination" ]
 
-  // content = $('#classroom_cards')    # where to load new content
-  // let content = this.scrollContainerTarget
-  // let viewMore = this.viewMoreTarget
-  // viewMore = $('#view-more') # tag containing the "View More" link
-
-
-  // let lastLoadAt = null          # when you loaded the last page
-  // let minTimeBetweenPages = 100 # milliseconds to wait between loading pages
-  // let loadNextPageAt = 100      # pixels above the bottom
-
-
-  connect(){
-
-  }
-
-  canary(){
-    this.approachingBottomOfPage()
-  }
-
-  approachingBottomOfPage() {
-    let domRect = this.scrollContainerTarget.getBoundingClientRect()
-    let spaceBelow =  domRect.bottom - window.innerHeight
-
-    if(spaceBelow < 100){
-      console.log("BOTTOM")
+  initialize() {
+    let options = {
+      rootMargin: '200px',
     }
-  };
 
-  waitedLongEnoughBetweenPages() {
-    return lastLoadAt === null || new Date() - lastLoadAt > minTimeBetweenPages;
-  };
-
-  nextPage() {
-    let url = this.viewMoreTarget.find('a').attr('href')
-    if (isLoadingNextPage || !url) {
-      return;
-    }
-    this.viewMoreTarget.addClass('loading')
-    isLoadingNextPage = true
-
+    this.intersectionObserver = new IntersectionObserver(entries => this.processIntersectionEntries(entries), options)
   }
+
+  connect() {
+    this.intersectionObserver.observe(this.paginationTarget)
+  }
+
+  disconnect() {
+    this.intersectionObserver.unobserve(this.paginationTarget)
+  }
+
+  processIntersectionEntries(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.loadMore()
+      }
+    })
+  }
+  loadMore() {
+    let next_page = this.paginationTarget.querySelector("a[rel='next']")
+    if (next_page == null) { return }
+    let url = next_page.href
+
+    Rails.ajax({
+      type: 'GET',
+      url: url,
+      dataType: 'json',
+      success: (data) => {
+        this.entriesTarget.insertAdjacentHTML('beforeend', data.entries)
+        this.paginationTarget.innerHTML = data.pagination
+      }
+    })
+  }
+
 
 }
-
-
-// (function() {
-//   $(function() {
-//     var approachingBottomOfPage, content, isLoadingNextPage, lastLoadAt, loadNextPageAt, minTimeBetweenPages, nextPage, viewMore, waitedLongEnoughBetweenPages;
-//     content = $('#classroom_cards');
-//     viewMore = $('#view-more');
-//     isLoadingNextPage = false;
-//     lastLoadAt = null;
-//     minTimeBetweenPages = 100;
-//     loadNextPageAt = 100;
-//     waitedLongEnoughBetweenPages = function() {
-//       return lastLoadAt === null || new Date() - lastLoadAt > minTimeBetweenPages;
-//     };
-//     approachingBottomOfPage = function() {
-//       return document.documentElement.clientHeight + $(document).scrollTop() < document.body.offsetHeight - loadNextPageAt;
-//     };
-//     nextPage = function() {
-//       var url;
-//       url = viewMore.find('a').attr('href');
-//       if (isLoadingNextPage || !url) {
-//         return;
-//       }
-//       viewMore.addClass('loading');
-//       isLoadingNextPage = true;
-//       lastLoadAt = new Date();
-//       return $.ajax({
-//         url: url,
-//         method: 'GET',
-//         dataType: 'script',
-//         success: function() {
-//           viewMore.removeClass('loading');
-//           isLoadingNextPage = false;
-//           return lastLoadAt = new Date();
-//         }
-//       });
-//     };
-//     $(window).scroll(function() {
-//       if (approachingBottomOfPage() && waitedLongEnoughBetweenPages()) {
-//         return nextPage();
-//       }
-//     });
-//     return viewMore.find('a').click(function(e) {
-//       nextPage();
-//       return e.preventDefaults();
-//     });
-//   });
-
-// }).call(this);
