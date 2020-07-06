383b841bd019e152a866596c38214139b65059e4
David Chmura <dschmura@humbledaisy.com>
2020-07-06 12:06:20 -0400

    ADD: Building serializer

---
+++ app/serializers/building_serializer.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: buildings
+#
+#  id           :bigint           not null, primary key
+#  bldrecnbr    :integer
+#  latitude     :float
+#  longitude    :float
+#  name         :string           not null
+#  nick_name    :string           not null
+#  abbreviation :string
+#  address      :string           not null
+#  city         :string           not null
+#  state        :string           not null
+#  zip          :string           not null
+#  country      :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class BuildingSerializer < ActiveModel::Serializer
+  attributes :id, :bldrecnbr, :latitude, :longitude, :name, :nick_name, :abbreviation, :address, :city, :state, :zip
+end
