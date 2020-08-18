3fb14d9c374ec02d851a33a6a34f7f7ec88969d1
David Chmura <dschmura@humbledaisy.com>
2020-08-18 11:28:00 -0400

    REFACTOR: Major reworking of map_controller
    ADDED: valid locations screen. Will return an array of locations that have latitudes and longitudes and disregard the rest.
    
    IN PROGRESS:  Eliminating duplicate markers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,3 @@
-console.log("Start")
 import { Controller } from "stimulus"
 import 'leaflet/dist/leaflet.css';
 import { get } from 'http'
@@ -10,28 +9,35 @@ export default class extends Controller {
   connect(){
     let mapBoxToken = this.data.get("mapbox-token");
     const incoming = JSON.parse(this.data.get("mapbox-location"));
-    const locations = Object.entries(incoming);
+    const locations_data = Object.entries(incoming.data);
+    const locations = this.validLocations(locations_data);
+
+    this.uniqueLocations(locations)
     this.createMap(locations, mapBoxToken);
+
+    let centerPoints = this.findCenterPoint(locations);
+    console.log("RESULTING CENTERPOINTS: " + centerPoints)
 
   }
 
   createMap(locations, token){
 
-    let centerPoints = this.findCenterPoint(locations);
+    // let centerPoints = this.findCenterPoint(locations);
 
-    if (centerPoints[0] != centerPoints[0]) {
+    // if (centerPoints[0] != centerPoints[0]) {
       let mymap = L.map('mapid').setView([42.277461, -83.738293], 13);
-      this.addTile(mymap, token);
-      this.resizeMap(mymap);
-    } else {
-      let mymap = L.map('mapid').setView([centerPoints[0], centerPoints[1]], 15);
       this.addTile(mymap, token);
       this.addMarkers(mymap, locations);
 
-      // this.resizeMap(mymap);
-    }
 
-    // this.resizeMap(mymap);
+    // } else {
+    //   let mymap = L.map('mapid').setView([centerPoints[0], centerPoints[1]], 15);
+    //   this.addTile(mymap, token);
+    //   this.addMarkers(mymap, locations);
+    //
+    // }
+
+      this.resizeMap(mymap);
 
   }
   addTile(map, token){
@@ -55,58 +61,121 @@ export default class extends Controller {
     setTimeout(function(){ map.invalidateSize()}, 0);
     map.getSize();
   }
-
   findCenterPoint(locations){
-    // 42.277083, -83.738224
-    let averageLatitude = 0;
-    let averageLongitude = 0;
-    let locationsCount = (locations.length ? 0 : 1);
+    var locationsCount = locations.length;
+    console.log("Count: " + locationsCount)
+    var X = 0.0;
+    var Y = 0.0;
+    var Z = 0.0;
 
-    locations[0][1].forEach(function(location, i){
-      if (location.attributes.building.latitude != null || location.attributes.building.longitude != null){
-        locationsCount++;
-        averageLatitude = averageLatitude + location.attributes.building.latitude;
-        averageLongitude = averageLongitude + location.attributes.building.longitude;
+    locations.forEach(function(location, i){
+      if (location[1].attributes.latitude && location[1].attributes.longitude){
+        var latitude = location[1].attributes.latitude * Math.PI / 180;
+        var longitude = location[1].attributes.longitude * Math.PI / 180;
+        var a = Math.cos(latitude) * Math.cos(longitude);
+        var b = Math.cos(latitude) * Math.sin(longitude);
+        var c = Math.sin(latitude);
 
+        X += a;
+        Y += b;
+        Z += c;
+
+        X /= locationsCount;
+        Y /= locationsCount;
+        Z /= locationsCount;
       }
-    });
-    averageLatitude = averageLatitude / locationsCount;
-    averageLongitude = averageLongitude / locationsCount;
+
+    var longitude_2 = Math.atan2(Y, X);
+    var hyp = Math.sqrt(X * X + Y * Y);
+    var latitude2 = Math.atan2(Z, hyp);
+
+    var averageLatitude = latitude2 * 180 / Math.PI;
+    var averageLongitude =  longitude_2 * 180 / Math.PI;
 
     let centerPoints = [averageLatitude, averageLongitude];
     return centerPoints;
+   });
   }
 
-  addMarker(map, location){
-    var mapIcon = L.icon({
-      iconUrl: mapMarker,
-      iconSize:     [30, 95],
-      className: 'map-marker-icon'
+  validLocations(locations){
+    // Locations need a latitude and longitude to be valid
+    let valid_locations = []
+    locations.forEach(function(location){
+
+      if (location[1].attributes.latitude != location[1].attributes.latitude || location[1].attributes.longitude != location[1].attributes.latitude){
+        valid_locations.push(location)
+      }
     });
-    var marker = L.marker([location[0], location[1]], {
-      icon: mapIcon
-    }).addTo(map);
+    console.log(valid_locations)
+    return valid_locations
+  }
+
+
+  uniqueLocations(locations) {
+    // return [...new Set(lat_long_pairs)];
+    let unique_locations = []
+    locations.forEach(function(location){
+      console.log(location[1].attributes.latitude, location[1].attributes.longitude)
+
+      if(unique_locations.includes(location[1].attributes.latitude, location[1].attributes.longitude)){
+        unique_locations.push(location[1].attributes.latitude, location[1].attributes.longitude)
+
+      }
+
+    })
+    console.log(unique_locations.length)
+    return unique_locations
+
+
+  }
+
+  addMarker(location){
+
+    // var mapIcon = L.icon({
+    //   iconUrl: mapMarker,
+    //   iconSize:     [30, 95],
+    //   className: 'map-marker-icon'
+    // });
+    // var marker = L.marker([location[0], location[1]], {
+    //   icon: mapIcon
+    // }).addTo(map);
 
   }
 
   addMarkers(map, locations){
-    let locationsCount = (locations.length ? 0 : 1);
+
+    // var loctions = this.uniqueLocations(locations)
+    var lat_long_pairs = []
     var mapIcon = L.icon({
       iconUrl: mapMarker,
+      iconSize:     [25],
       className: 'map-marker-icon'
     });
 
-    locations[0][1].forEach(function(location, i){
-      if (location.attributes.building.latitude != null || location.attributes.building.longitude != null){
-        locationsCount++;
-        let marker = L.marker([location.attributes.building.latitude, location.attributes.building.longitude], {
-          icon: mapIcon
-        }).addTo(map);
+    locations.forEach(function(location, i){
+      if (location[1].attributes.latitude != "" && location[1].attributes.longitude != "" ) {
+        lat_long_pairs.push(`${location[1].attributes.latitude}, ${location[1].attributes.longitude}`)
+      }
+    })
 
-        let popup = L.popup()
-        .setLatLng([location.attributes.building.latitude, location.attributes.building.longitude])
-        .setContent("I am a standalone popup.")
-        .openOn(map);
+
+    // console.log("BEFORE: " + lat_long_pairs.length)
+    // console.log("AFTER: " + this.uniqueLocations(lat_long_pairs).length)
+    // console.log(lat_long_pairs)
+
+
+    locations.forEach(function(location, i){
+      if (location[1].attributes.latitude != null || location[1].attributes.longitude != null){
+        console.log(location[1].attributes.location_name)
+        let marker = L.marker([location[1].attributes.latitude, location[1].attributes.longitude], {
+          icon: mapIcon
+        }).addTo(map).bindPopup(location[1].attributes.location_name);
+
+
+        // let popup = L.popup()
+        // .setLatLng([location.attributes.latitude, location.attributes.longitude])
+        // .setContent("I am a standalone popup.")
+        // .openOn(map);
 
 
       }
