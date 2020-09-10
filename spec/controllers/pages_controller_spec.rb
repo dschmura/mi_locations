1a6d62b2bba78294f865182fbb32289da9b67fef
David Chmura <dschmura@humbledaisy.com>
2020-09-10 13:40:55 -0400

    FIX: Pages controller specs to proper routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,28 +3,28 @@ require "rails_helper"
 RSpec.describe PagesController, type: :controller do
   describe "GET #about" do
     it "returns http success" do
-      get :about
+      get :about_path
       expect(response).to have_http_status(200)
     end
   end
 
   describe "GET #contact" do
     it "returns http success" do
-      get :contact
+      get :contact_path
       expect(response).to have_http_status(200)
     end
   end
 
   describe "GET #privacy" do
     it "returns http success" do
-      get :privacy
+      get :privacy_path
       expect(response).to have_http_status(200)
     end
   end
 
   describe "GET #project_status" do
     it "returns http success" do
-      get :project_status
+      get :project_status_path
       expect(response).to have_http_status(200)
     end
   end
